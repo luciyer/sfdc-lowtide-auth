fb7493c5eabce4b839dc23ddfec1827ea424d522
Luc Iyer <luc@alpha.local>
2020-09-24 22:45:26 -0300

    add redis_url

--- auth/session.js
+++ auth/session.js
@@ -4,9 +4,10 @@ const redis = require("redis")
 const session = require("express-session")
 
 let RedisStore = require("connect-redis")(session)
-let redisClient = redis.createClient()
+let redisClient = redis.createClient(process.env.REDIS_URL)
 
 const sessionOptions = {
+  name: "lowtide.auth",
   secret: process.env.SESSION_SECRET,
   cookie: { maxAge: (60 * 60000) },
   store: new RedisStore({ client: redisClient }),
